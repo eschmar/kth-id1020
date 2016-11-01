20f522a9f0ba375092bd1517f08b1f571123cb9f
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-11-01 18:51:03 +0100

    Import dependencies

--- src/io/eschmann/Main.java
+++ src/io/eschmann/Main.java
@@ -1,8 +1,11 @@
 package io.eschmann;
 
+import edu.princeton.cs.introcs.StdOut;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        StdOut.print("Pew!");
     }
 }
