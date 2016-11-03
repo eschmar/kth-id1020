102a25d9cbd8b5ed25cf22e5793b78da15b08d24
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-11-03 09:33:50 +0100

    Add module for lab001

---
+++ lab001/src/main/java/io/eschmann/Recursion.java
@@ -0,0 +1,12 @@
+package io.eschmann;
+
+import edu.princeton.cs.introcs.StdOut;
+
+/**
+ * Created by eschmar on 03/11/16.
+ */
+public class Recursion {
+    public static void main(String[] args) {
+        StdOut.println("Pew!");
+    }
+}
