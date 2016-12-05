eac2e3f2e931d8410e6a761646276d6578709fae
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-12-05 22:46:12 +0100

    Initial commit

---
+++ lab005-graph/src/main/java/io/eschmann/Paths.java
@@ -0,0 +1,14 @@
+package io.eschmann;
+
+/**
+ * Created by eschmar on 05/12/16.
+ */
+import se.kth.id1020.Graph;
+import se.kth.id1020.DataSource;
+
+public class Paths {
+    public static void main(String[] args) {
+        Graph g = DataSource.load();
+        // work on g
+    }
+}
